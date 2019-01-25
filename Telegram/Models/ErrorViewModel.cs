d65845b655bb20634a00db6cbeb340364cadead4
Кирилл Тюрин <Kirill-turin97@yandex.ru>
2019-01-25 13:01:54 +0300

    Add project files.

---
+++ Telegram/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Telegram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
